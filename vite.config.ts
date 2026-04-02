f4602bc25f7eba31ece60d2963f79254473aa841
Test <noel.marc.breuer1@gmail.com>
2026-04-02 13:07:32 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',
+  base: '/By-Noel/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
